db7820b5806524bfb39add8ea2b7930be2cbdd81
kalalgaurav18 <kalalgaurav18@gmail.com>
2022-08-19 21:23:54 +0530

    java calculator program added successfully

---
+++ Calculator.java
@@ -0,0 +1,50 @@
+public class Calculator {
+
+	public static double res;
+	public static double add(double a, double b) {
+
+		res = a+b;
+		return 0.0;
+	}
+     
+	public static double subt(double a, double b) {
+
+		res = a-b;
+		
+		return 0.0;
+	}
+	   
+	public static double mult(double a, double b) {
+
+		 res = a*b;
+		return 0.0;
+	}
+	
+	public static double div(double a, double b) {
+
+		res = a/b;
+		return 0.0;
+	}
+	public static double display() {
+
+		System.out.println("Result is : "+res);
+		return res;
+	}
+  
+	
+
+	public static void main(String[] args) {
+		double c;
+		Calculator cl = new Calculator();
+		cl.add(20, 30);
+		cl.display();
+		cl.subt(60,20);
+		cl.display();
+		cl.mult(96, 4);
+		cl.display();
+		cl.div(8, 4);
+		cl.display();
+		
+	
+	}
+}
